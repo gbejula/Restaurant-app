6dc94e7bd995c6125160debe28819013bee78863
Olugbenga Ajayi <gbejula@gmail.com>
2022-03-19 03:20:57 +0100

    completed navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,12 +14,25 @@ const Navbar = () => {
           />
         </div>
         <div className={styles.texts}>
-          <div className={styles.text}>ORDER NOW</div>
+          <div className={styles.text}>ORDER NOW!</div>
           <div className={styles.text}>123 456 789</div>
         </div>
       </div>
-      <div className={styles.item}>Center</div>
-      <div className={styles.item}>Right</div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Home</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <li className={styles.listItem}>Services</li>
+          <li className={styles.listItem}>Blogs</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src='/img/cart.png' alt='cart' width='30px' height='30px' />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
     </div>
   );
 };
